27ab5daed68899e757d8d250b4658afe1fb9775a
keebmack <139079928+keebmack@users.noreply.github.com>
2023-07-30 19:09:53 -0400

    Update scripts.js
    Adding feature to make background disappear when hovering over an image to remove distraction.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,23 @@
 $(document).ready(function() {
-    $(".enlarge-image").click(function() {
-      var imageSrc = $(this).attr("src");
-      var enlargedImage = $("<div class='enlarged-image-overlay'><img src='" + imageSrc + "'></div>");
-      $("body").append(enlargedImage);
-      enlargedImage.click(function() {
-        $(this).remove();
-      });
+  const bodyElement = $("body");
+  const galleryElement = $(".gallery");
+
+  galleryElement.on("mouseenter", ".enlarge-image", function() {
+    bodyElement.addClass("no-background");
+  });
+
+  galleryElement.on("mouseleave", ".enlarge-image", function() {
+    bodyElement.removeClass("no-background");
+  });
+
+  galleryElement.on("click", ".enlarge-image", function() {
+    var imageSrc = $(this).attr("src");
+    var enlargedImage = $("<div class='enlarged-image-overlay'><img src='" + imageSrc + "'></div>");
+    $("body").append(enlargedImage);
+
+    enlargedImage.click(function() {
+      $(this).remove();
+      bodyElement.removeClass("no-background"); // Restore background on image click
     });
   });
+});
